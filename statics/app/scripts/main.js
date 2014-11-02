1b516b3a622f43df607ab089dd7ace549961d2f3
esehara shigeo <esehara@gmail.com>
2014-11-02 16:23:51 +0900

    refresh: static files

--- statics/app/scripts/main.js
+++ statics/app/scripts/main.js
@@ -4,4 +4,7 @@ $(function() {
   $('.rainbow-background').pikapika({css: 'background-color'});
   $('.smooth-rainbow').pikapika({colorstep: 1});
   $('.zatsu-rainbow').pikapika({colorstep: 64});
+  $('.rainbow-range-r').pikapika(
+    {range_r: [128, 256], range_b: [0, 64], range_g: [0, 64]}
+  );
 });
